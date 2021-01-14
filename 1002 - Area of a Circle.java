200fec56fe04f6417cbc6fd80491d8b785601b5d
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-15 00:14:09 +0600

    1002 - Area of a Circle

---
+++ 1002 - Area of a Circle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double r = scan.nextDouble();
+		double area = 3.14159 * r *r;
+
+		System.out.printf("A=%.4f\n", area);
+
+	}
+
+}
