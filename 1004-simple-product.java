429c825f9b74fae1abff386c4ff4236e661a353e
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-16 17:37:05 +0600

    1004 - Simple Product

---
+++ 1004-simple-product.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		int C=A*B;
+		System.out.println("PROD = "+C);
+	}
+
+}
