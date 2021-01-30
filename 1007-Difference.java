43f4432c52a973f4d40d10963943e79625e3cfd9
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-30 21:19:20 +0600

    1007 - Difference

---
+++ 1007-Difference.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		int C = scan.nextInt();
+		int D = scan.nextInt();
+		int E = (A * B - C * D);
+		System.out.println("DIFERENCA = "+ E);
+		
+	}
+}
